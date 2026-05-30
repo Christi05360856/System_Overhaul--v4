743dc61d7961fb95360c9399836c2d06c5ae41d4
Christi05360856 <99998581+Christi05360856@users.noreply.github.com>
2026-05-30 07:07:28 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,8 @@
-// functions/src/index.ts
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as admin from 'firebase-admin';
+if (!admin.apps.length) { admin.initializeApp(); }
 
-// Initialize Firebase Admin
-admin.initializeApp();
-
-// Import our functions
-import { createQuizSession } from "./quiz/createSession";
-import { submitQuizSession } from "./quiz/submitSession";
-
-// Export all functions
-export {
-  createQuizSession,
-  submitQuizSession
-};
+export { createQuizSession }        from './quiz/createSession';
+export { submitQuizSession }        from './quiz/submitSession';
+export { processRewardClaim }       from './rewards/processReward';
+export { archiveWeeklyLeaderboard } from './leaderboard/archiveWeek';
+export { sendDailyReminders }       from './notifications/scheduler';
